574bb6d22a2457368166b4d9268aa89efd9b19f3
Lorimar Luis Castelan <162641424+Lorimarluis@users.noreply.github.com>
2024-04-03 21:51:33 -0300

    Add files via upload

---
+++ concatenaçao.js
@@ -0,0 +1,5 @@
+const nome = 'lorimar'
+const cor = 'azul'
+
+console.log(`meu nome é ${nome} e minha cor favorita é ${cor}.`)
+console.log('meu nome é ' + nome + 'e minha cor favorita é ' + cor +'.' )
